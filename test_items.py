91e5476186b6b9d36982ae59714fa645f49cd42d
NickerLog <Lobov2000@gmail.com>
2022-01-18 15:55:23 +0300

    adding comments to "wait" elements

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_lang_switch(browser):
-    browser.implicitly_wait(5)
+    browser.implicitly_wait(5)  # if page loading slowly (not needed if we're using time.sleep here)
     browser.get(link)
     time.sleep(15)  # time for look at browser window
     assert expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')), \
